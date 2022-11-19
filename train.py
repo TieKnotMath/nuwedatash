87772c1ddca69f2e7480d2d2d11a11192bc5c14f
TieKnotMath <72994729+TieKnotMath@users.noreply.github.com>
2022-11-19 23:22:03 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
-from models import resnet50, load_data
+from models import resnet50_custom, load_data
 import tensorflow as tf
 
 def train():
-    model = resnet50()
+    model = resnet50_custom()
     train_dataset, val_dataset, test_dataset = load_data()
     callback = tf.keras.callbacks.EarlyStopping(
     monitor="val_accuracy",
